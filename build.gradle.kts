537ffacb7176cd7547d0705018d553d20334197b
Damian Malczewski <damian.m.malczewski@gmail.com>
2025-09-23 23:22:24 +0200

    Remove not working publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,16 +54,6 @@ publishing {
             }
         }
     }
-    repositories {
-        maven {
-            name = "OSSRH"
-            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = project.findProperty("ossrhUsername") as String?
-                password = project.findProperty("ossrhPassword") as String?
-            }
-        }
-    }
 }
 
 spotless {
